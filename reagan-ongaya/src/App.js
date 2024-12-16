2302dfe6766ea1a06bd39ba7a6e3110c1023094f
Reagan-Ongaya <reagan.ongaya@student.moringaschool.com>
2024-12-16 23:50:09 +0300

    Blog updates

--- reagan-ongaya/src/App.js
+++ reagan-ongaya/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import {  HashRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
